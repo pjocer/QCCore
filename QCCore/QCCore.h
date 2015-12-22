3190363c08c14076c68edaeb6407438daa4cd30e
xuqian <xuqian@quancheng-ec.com>
2015-12-22 11:05:03 +0800

    create project

---
+++ QCCore/QCCore.h
@@ -0,0 +1,19 @@
+//
+//  QCCore.h
+//  QCCore
+//
+//  Created by XuQian on 12/22/15.
+//  Copyright © 2015 qcwl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QCCore.
+FOUNDATION_EXPORT double QCCoreVersionNumber;
+
+//! Project version string for QCCore.
+FOUNDATION_EXPORT const unsigned char QCCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QCCore/PublicHeader.h>
+
+
